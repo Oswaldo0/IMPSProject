2df451299aa4b20c012552e15e85f9e48a123d2f
Oswaldo <ac20i004001@usonsonate.edu.sv>
2023-11-12 20:59:32 -0600

    Mantenimiento estudiantes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars'); //necesario para utilizar el motor de pantillas handlebars
+const path = require('path');
 
 //Inicializaciones
 const app = express();
@@ -7,9 +10,24 @@ require('dotenv').config();
 
 //Ajustes del servidor 
 app.set('port', process.env.PORT || 4500);
+app.set('views', path.join(__dirname, 'views')); //configuracion de la ruta donde se encuentran las vistas
+
+app.engine('.hbs', exphbs.engine({
+    defaultLayout: 'main', //configuracion del layout principal
+    layoutsDir : path.join(app.get('views'), 'layouts'), //ruta de layout
+    extname: '.hbs'//extension que tendran los archivos HandleBars
+}));
+
+app.set('view engine', '.hbs'); //configuracion para ejecutar el motor de pantillas
+app.use(morgan('dev')); //configuracion middleware morgan para visualizar que esta llegando al servidor
+app.use(express.urlencoded({extended: false})); //sirve para aceptar datos desde formularios
 
 //Configurando rutas
 app.use(require('./routes')) //Node automaticamente buscara el index del modulo
+app.use('/estudiantes',require('./routes/estudiantes')); //configuracion de ruta para estudiantes
+
+//archivo publicos (Aca se coloca todo el codigo el cual el navegador puede acceder)
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Inciar el servidor
 app.listen(app.get('port'), () => {
--- src/repositories/EstudianteRepository.js
+++ src/repositories/EstudianteRepository.js
@@ -10,5 +10,36 @@ module.exports = {
         }catch(error){
             console.error('Ocurrio un problema al consultar estudiantes : ', error);
         }
+    },
+    
+    // Eliminar un estudiante
+
+eliminarEstudiante: async(idestudiante) => {
+    try{
+    const result = await pool.query('DELETE FROM estudiantes WHERE idestudiante = ?', [idestudiante]);
+    return result.affectedRows > 0;
+    }catch(error){
+    console.error('Erro al eliminar el registro', error);
     }
+    },
+
+    insertarNuevoEstudiante: async(idestudiante, nombre, apellido, email, idcarrera, usuario) =>{
+        try{
+            const result = await pool.query('INSERT INTO estudiantes(idestudiantes,nombre,apellido,email,idcarrera,carrera) VALUES (?,?,?,?,?,?)',[idestudiante,nombre,apellido,email,idcarrera,carrera]);
+            return result.affectedRows > 0;
+        }catch(error){
+            console.error('Ocurrio un error al ingresar el nuevo registro')
+        }
+    },
+
+    actualizarEstudiantes: async(idestudiante,nombre,apellido,email,idcarrera,carrera) =>{
+        try{
+            const result = await pool.query('UPDATE estudiantes SET idestudiante=?, nombre=?,apellido=?, email=?,idecarrera=?,carrera=? WHERE idestudiante=?',[idestudiante,nombre,apellido,email,idcarrera,carrera]);
+            return result.affectedRows > 0;
+        }catch(error){
+            console.error('No se pudo actualizar el registro');
+        }
+    }
+
+
 }
